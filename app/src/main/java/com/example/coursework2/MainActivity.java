e9fbac05eb4d34c7f4128255bb93935a70a963ea
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-11-28 20:39:57 +0000

    feat: created the about page after initilisation the widgets in the activitymain

--- app/src/main/java/com/example/coursework2/MainActivity.java
+++ app/src/main/java/com/example/coursework2/MainActivity.java
@@ -3,14 +3,26 @@ package com.example.coursework2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Toast;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mvSearch, acSearch, advSearch;
+    ImageView infoBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mvSearch = (Button) findViewById(R.id.btnMovieSearch);
+        acSearch = (Button) findViewById(R.id.btnActorSearch);
+        advSearch = (Button) findViewById(R.id.btnAdvancedSearch);
+        infoBtn = (ImageView) findViewById(R.id.ivInfo);
+
+
 
     }
 }
