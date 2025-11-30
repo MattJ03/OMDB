8d3414fef6f70f425d4a4d5e5165ffb7a1212cba
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-11-30 19:28:15 +0000

    feat: fixed mysaved movies file and added back button

--- app/src/main/java/com/example/coursework2/SavedMovieActivity.java
+++ app/src/main/java/com/example/coursework2/SavedMovieActivity.java
@@ -2,7 +2,9 @@ package com.example.coursework2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,8 +13,10 @@ import android.widget.TextView;
 import android.content.Intent;
 import com.example.coursework2.MovieEntity;
 import com.example.coursework2.MovieDao;
+import com.example.coursework2.MovieDatabase;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SavedMovieActivity extends AppCompatActivity {
 
@@ -30,9 +34,41 @@ public class SavedMovieActivity extends AppCompatActivity {
         listViewSaved = findViewById(R.id.listViewSaved);
         ivBack = findViewById(R.id.ivBack2);
 
+        movieDao = MovieDatabase.getInstance(this).movieDao();
+
+        loadSavedMovie();
+
+        ivBack.setOnClickListener(v -> {
+            Intent intent = new Intent(SavedMovieActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 
-    protected void getSavedMovies() {
+    private void loadSavedMovie() {
+        new Thread(() -> {
+            List<MovieEntity> movies = movieDao.getAllMovies();
 
+            if(movieTitles == null) {
+                movieTitles = new ArrayList<>();
+            } else {
+                movieTitles.clear();
+            }
+
+            for(MovieEntity m : movies) {
+                movieTitles.add(m.title + "(" + m.year + ")");
+            }
+
+            runOnUiThread(() -> {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                  SavedMovieActivity.this,
+                        R.layout.list_item_movie,
+                        R.id.tvMovieTitle,
+                        movieTitles
+                );
+                listViewSaved.setAdapter(adapter);
+            });
+        });
     }
+
+
 }
